10c8f81d423a31f2ebf07a71152797eb213afd7d
Andy Fillebrown <andy.fillebrown@gmail.com>
2015-10-31 12:33:46 -0400

    Add linux build script.

---
+++ build-linux.sh
@@ -0,0 +1,4 @@
+SRC_DIR=/home/andy/Projects/Csound-Instruments/src/-/instruments
+BIN_DIR=/home/andy/Projects/Csound-Instruments/bin/-/instruments
+cp $SRC_DIR/i-synth-1.csd $BIN_DIR/vi-synth-1.csd
+cp $SRC_DIR/i-synth-2.csd $BIN_DIR/vi-synth-2.csd
